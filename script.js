e632b875811dc0e2a083885098559f6b8c2390c7
Eason Peng <51696109+fred813jay@users.noreply.github.com>
2020-02-12 23:06:23 +0800

    20200212
    20200212

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var apiurl={
-  data:"https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json",
+  mask_data:"https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json",
+  road_data:"https://raw.githubusercontent.com/donma/TaiwanAddressCityAreaRoadChineseEnglishJSON/master/AllData.json"
 }
 
 var vm = new Vue({
@@ -8,62 +9,93 @@ var vm = new Vue({
   data: {
     classes: [],
     CountyName: [],
-    ZoneName: []
+    ZoneName: [],
+    road: []
   },
   mounted: function(){
     $.ajax({
-      url: apiurl.data,
+      url: apiurl.road_data,
       success: function(res){
-        vm.classes=JSON.parse(res);
-      }
+        var r  = JSON.parse(res)
+        for (let i=0 ; i< r.length ; i++){
+          if (i !==5 ){
+            if (i !==18){
+              console.log(i)
+              vm.road.push(r[i])
+            }
+          }
+        }
+      } 
+    })
+    $.ajax({
+      url: apiurl.mask_data,
+      success: function(res){
+        var rest = JSON.parse(res)
+        vm.classes=JSON.parse(res)
+        for (var i=0 ; i< rest["features"].length ; i++){
+          let a = rest["features"][i]["properties"].address
+          let b = a.replace(/巿/i, '市').replace(/臺/i, '台').replace(/淡/i,'新北市淡').replace(/桃園縣/i,'桃園市').replace(/高雄縣/i,'高雄市').replace(/彰化市/i,'彰化縣彰化市').replace(/台南縣/i,'台南市').replace(/屏東市/i,'屏東縣屏東市').replace(/花蓮市/i,'花蓮縣花蓮市').replace(/台東市/i,'台東縣台東市')  //更改錯字
+          vm.classes["features"][i]["properties"].address.replace(b)
+          // let c = b.indexOf('市')
+          // let d = b.indexOf('縣')
+          // if (d == -1){                                       //判斷為市
+          //   var addr = b.substring(c-2,c+1)
+          // }
+          // else if (c == -1){                                  //判斷為縣
+          //   var addr = b.substring(d-2,d+1)
+          // }
+          // else if (c == -1 && d == -1){                      
+          //   var addr = ""
+          // }
+          // let result = vm.CountyName.find(function(name, index, array){
+          //   return name === addr
+          // })
+          // if (result === undefined && addr.length>0){
+          //   vm.CountyName.push(addr)
+          //   console.log(i+'  '+addr)
+          // }
+        }
+      },
+
     })
   },
   methods: {
-    getCountyName: function(){
-<<<<<<< HEAD
-      for(var i=0; i<=this.classes["features"].length; i++){
-        console.log(i)
-        var a =  this.classes["features"][0]["properties"].name
-      //   // console.log(a)
-      //   // console.log(this.CountyName)
-      //   // // this.CountyName.push(a)
-      //   // this.CountyName = this.classes["features"][0]["properties"].name
-      }
-      // console.log(this.CountyName)
-=======
-      for (var i=0 ; i<this.classes["features"].length ; i++){
-        let a = this.classes["features"][i]["properties"].address
-        let b = a.replace(/巿/i, '市').replace(/臺/i, '台').replace(/淡/i,'新北市淡')  //更改錯字
-        let c = b.indexOf('市')
-        let d = b.indexOf('縣')
-        if (d == -1){                                       //判斷為市
-          var addr = b.substring(c-2,c+1)
-        }
-        else if (c == -1){                                  //判斷為縣
-          var addr = b.substring(d-2,d+1)
-        }
-        else if (c == -1 && d == -1){                      
-          var addr = ""
-        }
-        let result = this.CountyName.find(function(name, index, array){
-          return name === addr;   
-        });
-        if (result === undefined && addr.length>0){
-          this.CountyName.push(addr)
-          console.log(i+'  '+addr)
-        }
+    // getCountyName: function(){
+    //   for (var i=0 ; i<this.classes["features"].length ; i++){
+    //     let a = this.classes["features"][i]["properties"].address
+    //     let b = a.replace(/巿/i, '市').replace(/臺/i, '台').replace(/淡/i,'新北市淡')  //更改錯字
+    //     let c = b.indexOf('市')
+    //     let d = b.indexOf('縣')
+    //     if (d == -1){                                       //判斷為市
+    //       var addr = b.substring(c-2,c+1)
+    //     }
+    //     else if (c == -1){                                  //判斷為縣
+    //       var addr = b.substring(d-2,d+1)
+    //     }
+    //     else if (c == -1 && d == -1){                      
+    //       var addr = ""
+    //     }
+    //     let result = this.CountyName.find(function(name, index, array){
+    //       return name === addr;   
+    //     });
+    //     if (result === undefined && addr.length>0){
+    //       this.CountyName.push(addr)
+    //       console.log(i+'  '+addr)
+    //     }
 
-        // 以下為地區
-        // let zone = this.classes["features"][i]["properties"].address.substring(3,6)
-        // let result1 = this.ZoneName.find(function(name, index, array){
-        //   return name === zone;   
-        // });   
-        // if (result1 === undefined){
-        //   this.ZoneName.push(zone)
-        // }             
-      }
->>>>>>> ba427de676fb86b1f3362ba914160d531de9b49e
-    },     
+    //     // 以下為地區
+    //     // let zone = this.classes["features"][i]["properties"].address.substring(3,6)
+    //     // let result1 = this.ZoneName.find(function(name, index, array){
+    //     //   return name === zone;   
+    //     // });   
+    //     // if (result1 === undefined){
+    //     //   this.ZoneName.push(zone)
+    //     // }             
+    //   }
+    //   // if (this.CountyName.length == 0){
+    //   //   return
+    //   // }
+    // },     
     
   },
 })
